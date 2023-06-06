b826e9bf2d257f1fbb92dd20892292f3ae4da7fe
amasdeogratias <deogratiasprog@gmail.com>
2023-06-06 12:08:11 +0300

    updated admin panel setup

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -57,110 +57,7 @@
         <!-- ====================================
           ——— LEFT SIDEBAR WITH OUT FOOTER
         ===================================== -->
-        <aside class="left-sidebar bg-sidebar">
-          <div id="sidebar" class="sidebar sidebar-with-footer">
-            <!-- Aplication Brand -->
-            <div class="app-brand">
-              <a href="/index.html" title="Sleek Dashboard">
-                <svg
-                  class="brand-icon"
-                  xmlns="http://www.w3.org/2000/svg"
-                  preserveAspectRatio="xMidYMid"
-                  width="30"
-                  height="33"
-                  viewBox="0 0 30 33">
-                  <g fill="none" fill-rule="evenodd">
-                    <path class="logo-fill-blue" fill="#7DBCFF" d="M0 4v25l8 4V0zM22 4v25l8 4V0z" />
-                    <path class="logo-fill-white" fill="#FFF" d="M11 4v25l8 4V0z" />
-                  </g>
-                </svg>
-
-                <span class="brand-name text-truncate">FixPay</span>
-              </a>
-            </div>
-
-            <!-- begin sidebar scrollbar -->
-            <div class="" data-simplebar style="height: 100%;">
-              <!-- sidebar menu -->
-              <ul class="nav sidebar-inner" id="sidebar-menu">
-                <li class="has-sub active expand">
-                  <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#dashboard"
-                    aria-expanded="false" aria-controls="dashboard">
-                    <i class="mdi mdi-view-dashboard-outline"></i>
-                    <span class="nav-text">Dashboard</span> <b class="caret"></b>
-                  </a>
-
-                  <ul class="collapse show" id="dashboard" data-parent="#sidebar-menu">
-                    <div class="sub-menu">
-                      <li class="active">
-                        <a class="sidenav-item-link" href="index.html">
-                          <span class="nav-text">Ecommerce</span>
-                        </a>
-                      </li>
-
-                      <li class="">
-                        <a class="sidenav-item-link" href="analytics.html">
-                          <span class="nav-text">Analytics</span>
-                          <span class="badge badge-success">new</span>
-                        </a>
-                      </li>
-                    </div>
-                  </ul>
-                </li>
-
-                <li class="has-sub ">
-                  <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#app"
-                    aria-expanded="false" aria-controls="app">
-                    <i class="mdi mdi-pencil-box-multiple"></i>
-                    <span class="nav-text">App</span> <b class="caret"></b>
-                  </a>
-
-                  <ul class="collapse " id="app" data-parent="#sidebar-menu">
-                    <div class="sub-menu">
-                      <li class="">
-                        <a class="sidenav-item-link" href="chat.html">
-                          <span class="nav-text">Chat</span>
-                        </a>
-                      </li>
-
-                      <li class="">
-                        <a class="sidenav-item-link" href="contacts.html">
-                          <span class="nav-text">Contacts</span>
-                        </a>
-                      </li>
-
-                      <li class="">
-                        <a class="sidenav-item-link" href="team.html">
-                          <span class="nav-text">Team</span>
-                        </a>
-                      </li>
-
-                      <li class="">
-                        <a class="sidenav-item-link" href="calendar.html">
-                          <span class="nav-text">Calendar</span>
-                        </a>
-                      </li>
-                    </div>
-                  </ul>
-                </li>
-
-                <!-- <li class="section-title">
-                  UI Elements
-                </li> -->
-
-                <!-- <li class="section-title">
-                  Pages
-                </li> -->
-
-                <!-- <li class="section-title">
-                  Documentation
-                </li> -->
-              </ul>
-            </div>
-
-
-          </div>
-        </aside>
+        @include('admin.partials.sidebar')
 
 
           <!-- ====================================
@@ -169,59 +66,7 @@
         <div class="page-wrapper">
 
           <!-- Header -->
-          <header class="main-header " id="header">
-            <nav class="navbar navbar-static-top navbar-expand-lg">
-              <!-- Sidebar toggle button -->
-              <button id="sidebar-toggler" class="sidebar-toggle">
-                <span class="sr-only">Toggle navigation</span>
-              </button>
-              <!-- search form -->
-              <div class="search-form d-none d-lg-inline-block">
-                <div class="input-group">
-                  <button type="button" name="search" id="search-btn" class="btn btn-flat">
-                    <i class="mdi mdi-magnify"></i>
-                  </button>
-                  <input type="text" name="query" id="search-input" class="form-control" placeholder="search..."
-                    autofocus autocomplete="off" />
-                </div>
-                <div id="search-results-container">
-                  <ul id="search-results"></ul>
-                </div>
-              </div>
-
-              <div class="navbar-right ">
-                <ul class="nav navbar-nav">
-
-                  <!-- User Account -->
-                  <li class="dropdown user-menu">
-                    <button href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
-                      <img src="{{('backend/assets/img/user/user.png')}}" class="user-image" alt="User Image" />
-                      <span class="d-none d-lg-inline-block">Deo amas</span>
-                    </button>
-                    <ul class="dropdown-menu dropdown-menu-right">
-                      <!-- User image -->
-                      <li class="dropdown-header">
-                        <img src="{{('backend/assets/img/user/user.png')}}" class="img-circle" alt="User Image" />
-                        <div class="d-inline-block">
-                          Deo Amas <small class="pt-1">iamdeo@gmail.com</small>
-                        </div>
-                      </li>
-
-                      <li>
-                        <a href="user-profile.html">
-                          <i class="mdi mdi-account"></i> My Profile
-                        </a>
-                      </li>
-
-                      <li class="dropdown-footer">
-                        <a href="index.html"> <i class="mdi mdi-logout"></i> Log Out </a>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-              </div>
-            </nav>
-          </header>
+          @include('admin.partials.header')
 
 
           <!-- ====================================
@@ -230,7 +75,7 @@
           <div class="content-wrapper">
             <div class="content">
 
-@yield('content')
+            @yield('content')
 
 
 
