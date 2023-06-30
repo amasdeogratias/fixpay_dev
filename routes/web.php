fbe7e5d4c2a10ef129de0a0c650daaf43eddc3b5
amasdeogratias <deogratiasprog@gmail.com>
2023-06-30 13:45:36 +0300

    added default use authentication

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 });
 
 Route::view('/admin', 'admin.dashboard.index');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
