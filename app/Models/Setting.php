61dbade4f0a9e9244490d98e4cd875f96873a91e
amasdeogratias <deogratiasprog@gmail.com>
2023-07-11 11:40:51 +0300

    select setting key and update given key value in the database

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -4,10 +4,36 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Config;
 
 class Setting extends Model
 {
     protected $table = 'settings';
 
     protected $fillable = ['key', 'value'];
+
+    //query setting value by key
+    public function get($key)
+    {
+        $setting = new self();
+        $entry = $setting->where('key', $key)->first();
+        if(!$entry){
+            return;
+        }
+        return $entry->value;
+    }
+
+    //update existing key value
+    public function set($key, $value=null)
+    {
+        $setting = new self();
+        $entry = $setting->where('key', $key)->firstOrFail();
+        $entry->value = $value;
+        $entry->saveOrFail();
+        Config::set('key', $value);
+        if (Config::get($key) == $value) {
+            return true;
+        }
+        return false;
+    }
 }
