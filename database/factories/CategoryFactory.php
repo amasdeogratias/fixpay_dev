39f82704c78a21c07bd5ae264cf4b0f6e4528636
amasdeogratias <deogratiasprog@gmail.com>
2023-07-12 15:45:17 +0300

    added category seeder and factory class

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Generator as Faker;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+$factory->define(Category::class, function (Faker $faker) {
+    return [
+        'name'          =>  $faker->name,
+        'description'   =>  $faker->realText(100),
+        'parent_id'     =>  1,
+        'menu'          =>  1,
+    ];
+});
