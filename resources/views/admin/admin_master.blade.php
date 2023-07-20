9a2d023efe4ed92f0095e8847d1b46745793cc2a
amasdeogratias <deogratiasprog@gmail.com>
2023-07-20 17:16:26 +0300

    Added javascript function for Datatable

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -89,5 +89,17 @@
            });
         });
     </script>
+    <script>
+        $(function () {
+          $('#example2').DataTable({
+            "paging": true,
+            "lengthChange": true,
+            "searching": true,
+            "ordering": true,
+            "info": true,
+            "autoWidth": false,
+          });
+        });
+      </script>
 </body>
 </html>
