7b07572adad87b48d837d75a4b431bd2ecb90f28
fixnetcodes <deogratiasprog@gmail.com>
2023-08-02 11:06:29 +0300

    added edit form and function for attributes

--- app/Http/Controllers/Admin/AttributeController.php
+++ app/Http/Controllers/Admin/AttributeController.php
@@ -43,4 +43,12 @@ class AttributeController extends BaseController
         }
         return $this->responseRedirect('admin.attributes.create', 'Attribute added successfully' ,'success',false, false);
     }
+
+    public function edit($id)
+    {
+        $attribute = $this->attributeRepository->findAttributeById($id);
+        $this->setPageTitle('Attributes', 'Edit Attribute: '.$attribute->name);
+        return view('admin.attributes.edit', compact('attribute'));
+
+    }
 }
---
+++ resources/views/admin/attributes/edit.blade.php
@@ -0,0 +1,89 @@
+@extends('admin.admin_master')
+@section('title') {{ $pageTitle }} @endsection
+
+@section('content')
+
+<!-- Main content -->
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0 text-dark">{{ $pageTitle }}</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="{{route('admin.dashboard')}}">Home</a></li>
+                </ol>
+            </div>
+        </div><!-- /.row -->
+    </div>
+
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card card-primary card-outline">
+                        <div class="card-header">
+                            <h3 class="card-title">{{ $subTitle }}</h3>
+                            <a href="{{route('admin.attributes.index')}}" class="btn btn-success float-right"><i class="fa fa-list"></i> View Attributes</a>
+                        </div>
+                        <div class="card-body">
+                            <form action="{{ route('admin.attributes.update') }}" method="post" role="form">
+                                @csrf
+                                <div class="row">
+                                    <div class="form-group col-md-6">
+                                        <label for="code">Code</label>
+                                        <input type="text" name="code" id="code" value="{{ $attribute->code }}" placeholder="Enter Attribute Code" class="form-control">
+                                        <input type="hidden" name="id" value="{{ $attribute->id }}">
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="name">Name</label>
+                                        <input type="text" name="name" id="name" value="{{ $attribute->name }}" placeholder="Enter Attribute Name" class="form-control">
+                                    </div>
+                                    <div class="form-group col-md-12">
+                                        <label class="control-label" for="frontend_type">Frontend Type</label>
+                                        @php $types = ['select' => 'Select Box', 'radio' => 'Radio Button', 'text' => 'Text Field', 'text_area' => 'Text Area']; @endphp
+                                        <select name="frontend_type" id="frontend_type" class="form-control">
+                                            @foreach($types as $key => $label)
+                                                @if($attribute->frontend_type == $key)
+                                                    <option value="{{ $key }}" selected>{{ $label }}</option>
+                                                @else
+                                                    <option value="{{ $key }}">{{ $label }}</option>
+                                                @endif
+
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="form-group col-md-12">
+                                        <div class="form-check">
+                                            <label class="form-check-label">
+                                                <input class="form-check-input" type="checkbox" id="is_filterable" name="is_filterable" {{ $attribute->is_filterable == 1 ? 'checked' : '' }}/>Filterable
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group col-md-12">
+                                        <div class="form-check">
+                                            <label class="form-check-label">
+                                                <input class="form-check-input" type="checkbox" id="is_required" name="is_required" {{ $attribute->is_required == 1 ? 'checked' : '' }}/>Required
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card-footer">
+                                    <div class="row d-print-none mt-2">
+                                        <div class="col-12 text-right">
+                                            <button class="btn btn-success" type="submit"><i class="fa fa-fw fa-lg fa-check-circle"></i>Save Attribute</button>
+                                            <a class="btn btn-danger" href="{{ route('admin.attributes.index') }}"><i class="fa fa-fw fa-lg fa-arrow-left"></i>Go Back</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+@endsection
