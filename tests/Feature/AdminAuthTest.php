63c0d9607c8f1c4d07ab94c250cfde60a6b5f130
fixnetcodes <deogratiasprog@gmail.com>
2023-08-05 11:20:39 +0300

    added test to test all admin routes protected by auth admin middleware

--- tests/Feature/AdminAuthTest.php
+++ tests/Feature/AdminAuthTest.php
@@ -5,7 +5,10 @@ namespace Tests\Feature;
 use App\Models\Admin;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Routing\Route;
+use Illuminate\Routing\Router;
 use Tests\TestCase;
+
 
 class AdminAuthTest extends TestCase
 {
@@ -25,5 +28,28 @@ class AdminAuthTest extends TestCase
 
         $response->assertStatus(302);
         $response->assertRedirect('/admin');
+    }
+
+    
+    public function test_admin_routes_protected_by_admin_middleware()
+    {
+        /** @var Router $router */
+        $router = app('router');
+
+
+        /** @var Collection|Route[] $routes */
+        $unprotected_routes = collect($router->getRoutes())->filter(function (Route $route) {
+            if (!preg_match('/^admin/', $route->getName())) {
+                return false;
+            }
+
+            return !in_array('auth:admin', $route->gatherMiddleware());
+        });
+
+        $message = $unprotected_routes->map(function(Route $route){
+            return sprintf('Route `%s` (%s) is missing `auth` middleware', $route->uri(), $route->getName());
+        })->implode("\n");
+
+        $this->assertCount(0, $unprotected_routes, $message);
     }
 }
