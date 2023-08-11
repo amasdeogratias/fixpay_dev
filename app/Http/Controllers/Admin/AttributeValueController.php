d96b3dbd074b1ac7e3cfc9db652f51986c003ec5
fixnetcodes <deogratiasprog@gmail.com>
2023-08-11 11:22:25 +0300

    returned all attribute values by attribute id in a controller

---
+++ app/Http/Controllers/Admin/AttributeValueController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Contracts\AttributeContract;
+use App\Models\AttributeValue;
+
+class AttributeValueController extends Controller
+{
+    protected $attributeRepository;
+
+    public function __construct(AttributeContract $attributeRepository)
+    {
+        $this->attributeRepository = $attributeRepository;
+    }
+
+    //get all attributes values by id
+    public function getValues(Request $request)
+    {
+        $attributeId = $request->input('id');
+        $attribute = $this->attributeRepository->findAttributeById($attributeId);
+
+        $values = $attribute->values; //return all attributes values associated with attribute id/model
+        return response()->json($value);
+    }
+}
