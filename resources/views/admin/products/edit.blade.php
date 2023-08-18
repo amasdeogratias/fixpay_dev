8f76114bef9258c73649754bf8b5430761cfe230
fixnetcodes <deogratiasprog@gmail.com>
2023-08-18 11:26:48 +0300

    added dropzone script for drag and drop image during upload

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -2,7 +2,7 @@
 @section('title') {{ $pageTitle }} @endsection
 
 @section('styles')
-    <link rel="stylesheet" type="text/css" href="{{ asset('backend/js/plugins/dropzone/dist/min/dropzone.min.css') }}"/>
+<link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
 @endsection
 @section('content')
 
@@ -256,6 +256,50 @@
     $('.select2').select2()
     });
 </script>
-<script type="text/javascript" src="{{ asset('backend/js/plugins/dropzone/dist/min/dropzone.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('backend/js/plugins/bootstrap-notify.min.js') }}"></script>
+<script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>
+
+
+<script>
+    Dropzone.autoDiscover = false;
+
+    $( document ).ready(function() {
+        $('#categories').select2();
+
+        let myDropzone = new Dropzone("#dropzone", {
+            paramName: "image",
+            addRemoveLinks: false,
+            maxFilesize: 4,
+            parallelUploads: 2,
+            uploadMultiple: false,
+            url: "{{ route('admin.products.images.upload') }}",
+            autoProcessQueue: false,
+        });
+        myDropzone.on("queuecomplete", function (file) {
+            window.location.reload();
+            showNotification('Completed', 'All product images uploaded', 'success', 'fa-check');
+        });
+        $('#uploadButton').click(function(){
+            if (myDropzone.files.length === 0) {
+                showNotification('Error', 'Please select files to upload.', 'danger', 'fa-close');
+            } else {
+                myDropzone.processQueue();
+            }
+        });
+        function showNotification(title, message, type, icon)
+        {
+            $.notify({
+                title: title + ' : ',
+                message: message,
+                icon: 'fa ' + icon
+            },{
+                type: type,
+                allow_dismiss: true,
+                placement: {
+                    from: "top",
+                    align: "right"
+                },
+            });
+        }
+    });
+</script>
 @endpush
