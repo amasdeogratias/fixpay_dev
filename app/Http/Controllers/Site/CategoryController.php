31efa85f3836bfd5cb529ecc96bf999c164c8b9d
fixnetcodes <deogratiasprog@gmail.com>
2023-08-24 17:14:43 +0300

    added product list by categories

--- app/Http/Controllers/Site/CategoryController.php
+++ app/Http/Controllers/Site/CategoryController.php
@@ -17,7 +17,6 @@ class CategoryController extends Controller
     public function show($slug)
     {
         $category = $this->categoryRepository->findBySlug($slug);
-        dd($category);
         return view('site.pages.category', compact('category'));
     }
 }
