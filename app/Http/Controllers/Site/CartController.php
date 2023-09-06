6123e57c75ffb264a37fb9ef5f025190d670df17
fixnetcodes <deogratiasprog@gmail.com>
2023-09-06 15:14:49 +0300

    added functionality for cearing and deleting item in cart

--- app/Http/Controllers/Site/CartController.php
+++ app/Http/Controllers/Site/CartController.php
@@ -12,4 +12,21 @@ class CartController extends Controller
     {
         return view('site.pages.cart');
     }
+
+    public function removeItem($id)
+    {
+        Cart::remove($id);
+
+        if (Cart::isEmpty()) {
+            return redirect('/');
+        }
+        return redirect()->back()->with('message', 'Item removed from cart successfully.');
+    }
+
+    public function clearCart()
+    {
+        Cart::clear();
+
+        return redirect('/');
+    }
 }
