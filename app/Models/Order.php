98525022d3a4b1bbbaacd11db195e2b6f9a97ce5
fixnetcodes <deogratiasprog@gmail.com>
2023-09-12 14:37:49 +0300

    added order and order items model and migration files and their relationship

---
+++ app/Models/Order.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+    protected $table = 'orders';
+
+    protected $fillable = [
+        'order_number', 'user_id', 'status', 'grand_total', 'item_count', 'payment_status', 'payment_method',
+        'first_name', 'last_name', 'address', 'city', 'country', 'post_code', 'phone_number', 'notes'
+    ];
+
+    /**
+     * Get the user that owns the Order
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    /**
+     * Get all of the items for the Order
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
---
+++ app/Models/OrderItem.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItem extends Model
+{
+    use HasFactory;
+    protected $table = 'order_items';
+
+    protected $fillable = [
+        'order_id', 'product_id', 'quantity', 'price'
+    ];
+
+    /**
+     * Get the product that owns the OrderItem
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+}
